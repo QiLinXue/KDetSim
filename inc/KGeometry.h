611a93f5d6eb35a28e581d0c02522bd3035138bd
QiLin Xue <xueqilin@gmail.com>
2021-05-31 17:39:34 -0400

    formatting

--- inc/KGeometry.h
+++ inc/KGeometry.h
@@ -4,51 +4,53 @@
 #include "TH3.h"
 #include "TH2.h"
 
-TH2D * KHisProject( void *,Int_t , Int_t );
+TH2D *KHisProject(void *, Int_t, Int_t);
 int GetNhs();
 
-class KGeometry {
+class KGeometry
+{
 
- private:
-
- public:
-
-  TH3I * EG;  //electrode geometry
-  TH3I * DM;  //detector material
-  Int_t nx;   //x-divisions
-  Int_t ny;   //y-divisions
-  Int_t nz;   //z-divisions 
+private:
+public:
+  TH3I *EG; //electrode geometry
+  TH3I *DM; //detector material
+  Int_t nx; //x-divisions
+  Int_t ny; //y-divisions
+  Int_t nz; //z-divisions
 
   // Constructors of the class
   KGeometry();
   ~KGeometry();
-  KGeometry( TH3I * x ){ GetGrid( x, 0 ); };
-  KGeometry( TH3I * x, TH3I * y ){ GetGrid( x, 0 ); GetGrid( x, 1 ); };
+  KGeometry(TH3I *x) { GetGrid(x, 0); };
+  KGeometry(TH3I *x, TH3I *y)
+  {
+    GetGrid(x, 0);
+    GetGrid(x, 1);
+  };
 
   // Setting up grids
-  void GetGrid( TH3I *, Short_t = 0 ); 
+  void GetGrid(TH3I *, Short_t = 0);
   Int_t SetBoundaryConditions();
-  inline int SetElecVolt(int i){ return( (i<<16)|32768 ); };
-  Double_t GetStepSize( Int_t, Int_t );
-  Double_t GetStepSize( Int_t, Float_t );
+  inline int SetElecVolt(int i) { return ((i << 16) | 32768); };
+  Double_t GetStepSize(Int_t, Int_t);
+  Double_t GetStepSize(Int_t, Float_t);
 
   // Mapping and projections
   //  TH2D *ProjectToGeometry(void *,Int_t,Int_t);
   //  Double_t InterpolateToGeometry();
-  TH3D * MapToGeometry( Double_t *, Double_t = 1 );
-  TH3D * GetGeom();
-  void Reset( Int_t = 0, Int_t = 0 );
+  TH3D *MapToGeometry(Double_t *, Double_t = 1);
+  TH3D *GetGeom();
+  void Reset(Int_t = 0, Int_t = 0);
 
   // Electrodes generators
   void ElRectangle(Float_t *Pos, Float_t *Size, Int_t Wei, Int_t Mat);
-  void ElLine(Float_t *r0,Float_t *r1, Float_t *W, Int_t Wei, Int_t Mat);
-  void ElCylinder(Float_t *Pos,Float_t R, Float_t L,Int_t O, Int_t Wei, Int_t Mat); 
+  void ElLine(Float_t *r0, Float_t *r1, Float_t *W, Int_t Wei, Int_t Mat);
+  void ElCylinder(Float_t *Pos, Float_t R, Float_t L, Int_t O, Int_t Wei, Int_t Mat);
 
   Float_t GetLowEdge(Int_t);
   Float_t GetUpEdge(Int_t);
 
-  ClassDef(KGeometry,1);
-
+  ClassDef(KGeometry, 1);
 };
 
 #endif
